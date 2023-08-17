6279f5ea364005ea13c13a5b2760a561c64b3406
wangyang <1184024857@qq.com>
2023-08-17 17:30:05 +0800

    1

--- helloflutter/lib/main.dart
+++ helloflutter/lib/main.dart
@@ -32,11 +32,40 @@ class HomePage extends StatelessWidget{
   }
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child: Column(
+        children: <Widget>[
+          ProductItem("Apple1", "Macbook Product1", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg"),
+          ProductItem("Apple2", "Macbook Product2", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imm9u5zj30u00k0adf.jpg"),
+          ProductItem("Apple3", "Macbook Product3", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imqlouhj30u00k00v0.jpg"),
+        ],
+      ),
+    );
+  }
+}
+
+//列表成员
+class ProductItem extends StatelessWidget {
+  final String title;
+  final String desc;
+  final String imageURL;
+  const ProductItem(this.title, this.desc, this.imageURL, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: <Widget>[
+          Text(title),
+          Text(desc),
+          Image.network(imageURL)
+        ],
+      ),
+    );
   }
 }
