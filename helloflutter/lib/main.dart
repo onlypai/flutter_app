9c3ed2d2e5dba729f57028a338a0e0ebfd5e8bad
wangyang <1184024857@qq.com>
2023-08-18 17:07:04 +0800

    qw

--- helloflutter/lib/main.dart
+++ helloflutter/lib/main.dart
@@ -37,8 +37,8 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
+    return Center(
+      child: ListView(
         children: <Widget>[
           ProductItem("Apple1", "Macbook Product1", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72j6nk1d4j30u00k0n0j.jpg"),
           ProductItem("Apple2", "Macbook Product2", "https://tva1.sinaimg.cn/large/006y8mN6gy1g72imm9u5zj30u00k0adf.jpg"),
@@ -49,7 +49,7 @@ class Body extends StatelessWidget {
   }
 }
 
-//列表成员
+//列表成员ProductItem
 class ProductItem extends StatelessWidget {
   final String title;
   final String desc;
@@ -58,7 +58,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         children: <Widget>[
           Text(title),
